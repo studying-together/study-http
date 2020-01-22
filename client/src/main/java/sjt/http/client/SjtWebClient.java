4c0d871d2458484dafb8fd85dda857094665832b
윤현준 <homelus@daum.net>
2020-01-22 17:27:11 +0900

    init server & client

---
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -0,0 +1,20 @@
+package sjt.http.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SjtWebClient {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("127.0.0.1", 8081);
+
+        final OutputStream out = socket.getOutputStream();
+        out.write("GET /index.html HTTP/1.1".getBytes());
+
+
+        socket.close();
+    }
+
+}
---
+++ server/src/main/java/sjt/http/client/SjtWebServer.java
@@ -0,0 +1,21 @@
+package sjt.http.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SjtWebServer {
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 8081;
+        ServerSocket serverSocket = new ServerSocket(port);
+
+    }
+
+
+
+}
