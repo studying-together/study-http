22bad544974796a539090bd798bc0797a4742048
윤현준 <homelus@daum.net>
2020-02-22 14:38:45 +0900

    echo client & server

--- client/src/main/java/sjt/http/client/SjtWebClient.java
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -1,6 +1,7 @@
 package sjt.http.client;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -8,13 +9,27 @@ public class SjtWebClient {
 
     public static void main(String[] args) throws IOException {
 
+        System.out.println("[CLIENT] start");
         Socket socket = new Socket("127.0.0.1", 8081);
 
+        final InputStream in = socket.getInputStream();
         final OutputStream out = socket.getOutputStream();
+        StringBuilder response = new StringBuilder();
+
         out.write("GET /index.html HTTP/1.1".getBytes());
+        out.write(0);
 
+        while (true) {
+            int c = in.read();
+            if (c <= 0) {
+                break;
+            }
+            response.append((char) c);
+        }
 
+        System.out.println("[CLIENT] response : " + response.toString());
         socket.close();
+        System.out.println("[CLIENT] end");
     }
 
 }
--- server/src/main/java/sjt/http/client/SjtWebServer.java
+++ server/src/main/java/sjt/http/client/SjtWebServer.java
@@ -2,6 +2,7 @@ package sjt.http.client;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -10,10 +11,29 @@ import java.util.concurrent.Executors;
 public class SjtWebServer {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("[SERVER] connection start");
 
         int port = 8081;
         ServerSocket serverSocket = new ServerSocket(port);
 
+        Socket socket = serverSocket.accept();
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            int c = in.read();
+            if (c <= 0) {
+                break;
+            }
+            sb.append((char) c);
+        }
+
+        System.out.println("in : " + sb.toString());
+        out.write(sb.toString().getBytes());
+        out.write(0);
+
+        System.out.println("[SERVER] connection end");
     }
 
 
