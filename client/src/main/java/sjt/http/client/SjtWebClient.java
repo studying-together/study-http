1e8753e789f4e20ed97e45162f48a664663bf224
김다희 <sefide36@tmon.co.kr>
2020-02-25 22:01:08 +0900

    feat: 1차 구조 생성

--- client/src/main/java/sjt/http/client/SjtWebClient.java
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -1,20 +1,50 @@
 package sjt.http.client;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
 public class SjtWebClient {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        Socket socket = new Socket("127.0.0.1", 8081);
+        // 서버 접속요청
+        Socket socket = new Socket("127.0.0.1", 8080);
+        InputStream inputStream = socket.getInputStream();
+        OutputStream outputStream = socket.getOutputStream();
 
-        final OutputStream out = socket.getOutputStream();
-        out.write("GET /index.html HTTP/1.1".getBytes());
+        // 서버에게 메세지 보내기
+        sendRequestMessage(outputStream);
 
+        Thread.sleep(3000);
+        // 메세지 받기
+        getResponseMessage(inputStream);
 
-        socket.close();
+        // 종료
+        Thread.sleep(1000);
+
+//        inputStream.close();
+//        outputStream.close();
+//        socket.close();
+        System.out.println("client close");
+    }
+
+    private static void getResponseMessage(InputStream inputStream) throws IOException {
+        System.out.println("get Message.. - " + System.currentTimeMillis());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        System.out.println(bufferedReader.readLine());
+    }
+
+    private static void sendRequestMessage(OutputStream outputStream) throws IOException {
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+        bufferedOutputStream.write("GET /index.html HTTP/1.1".getBytes());
+        bufferedOutputStream.flush();
+        System.out.println("send Message..");
+        bufferedOutputStream.close();
     }
 
 }
--- server/src/main/java/sjt/http/server/SjtWebServer.java
+++ server/src/main/java/sjt/http/server/SjtWebServer.java
@@ -1,21 +1,79 @@
-package sjt.http.client;
+package sjt.http.server;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class SjtWebServer {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        int port = 8081;
+        int port = 8080;
+
+        // 클라이언트 요청받을 준비
         ServerSocket serverSocket = new ServerSocket(port);
 
+        // 클라이언트 요청 받기
+        Socket socket = serverSocket.accept();
+        InputStream inputStream = socket.getInputStream();
+        OutputStream outputStream = socket.getOutputStream();
+
+        // 클라이언트 요청 기다리기
+        Thread.sleep(1000);
+
+        // 클라이언트가 보낸 데이터 확인 및 보관
+        String message = getRequestMessageByBuffredReader(inputStream);
+
+        if(message != null){
+            String[] messages = message.split(" ");
+            // 클라이언트에게 메세지 보내기
+            sendResponseMessage(outputStream, messages);
+        }
+
+        // 종료
+        Thread.sleep(9000);
+
+//        inputStream.close();
+//        outputStream.close();
+//        socket.close();
+        System.out.println("server close - " + System.currentTimeMillis());
     }
 
+    private static void sendResponseMessage(OutputStream outputStream, String[] message) throws IOException {
+        System.out.println("sendResponseMessage open.. ");
 
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+        bufferedWriter.write("200 OK ! " + message[0]);
+        bufferedWriter.flush();
+        System.out.println("sendResponseMessage closed.. ");
+        bufferedWriter.close();
+    }
+
+    private static String getRequestMessageByBuffredReader(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        System.out.println("getMessageByBuffredReader open.. ");
+
+        String message = bufferedReader.readLine();
+        System.out.println(message);
+        System.out.println("getMessageByBuffredReader closed.. ");
+        return message;
+    }
+
+    private static void getRequestMessageByBufferedInputStream(InputStream inputStream) throws IOException {
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+        while(bufferedInputStream.available() > 0) {
+            System.out.print(Character.toString((char)bufferedInputStream.read()));
+        }
+        System.out.println();
+        System.out.println("getMessageByBufferedInputStream close.. ");
+    }
 
 }
