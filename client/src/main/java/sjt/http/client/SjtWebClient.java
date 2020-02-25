368f32b8f3f663f2df6c62f0f2e1302e493e2f6a
김다혜 <dahye432111@gmail.com>
2020-02-25 22:29:24 +0900

    step1: http client/server 만들어보기
    client 에서 request 보내고 server에서 response를 보내 log로 남겨보기

--- client/src/main/java/sjt/http/client/SjtWebClient.java
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -1,20 +1,34 @@
 package sjt.http.client;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
+@Slf4j
 public class SjtWebClient {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
+        try {
+            Socket socket = new Socket("127.0.0.1", 8081); // 통신할 server socket
 
         Socket socket = new Socket("127.0.0.1", 8081);
 
-        final OutputStream out = socket.getOutputStream();
-        out.write("GET /index.html HTTP/1.1".getBytes());
+            final OutputStream out = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(out, true);
+            writer.println("GET /index.html HTTP/1.1");
 
+            final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //response : server 응답을 받을 입력 스트림
 
-        socket.close();
+            log.info("response : " + in.readLine());
+
+            socket.close();
+        } catch (UnknownHostException e) {
+            log.error("not found host : " + e);
+        } catch (IOException e) {
+            log.error("i/o exception" + e);
+        }
     }
 
 }
--- server/src/main/java/sjt/http/server/SjtWebServer.java
+++ server/src/main/java/sjt/http/server/SjtWebServer.java
@@ -1,21 +1,29 @@
-package sjt.http.client;
+package sjt.http.server;
 
-import java.io.IOException;
-import java.io.InputStream;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
+@Slf4j
 public class SjtWebServer {
-
     public static void main(String[] args) throws IOException {
 
         int port = 8081;
-        ServerSocket serverSocket = new ServerSocket(port);
+        ServerSocket serverSocket = new ServerSocket(port); // 서버 생성을 위한 serversocket
 
+        Socket client = serverSocket.accept(); // client와 통신하기 위한 socket, client 접속 대기
+
+        final BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        String clientRequest = in.readLine();
+
+        log.info("client request: " + clientRequest);
+
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true); // 출력 스트림 만들기
+        out.write("HTTP/1.1 200 OK");
+
+        client.close();
     }
-
-
-
 }
