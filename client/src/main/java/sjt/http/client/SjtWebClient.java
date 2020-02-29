ffdb00bdd762f6aa400701ecf28ca4f78d8fc4ae
김다희 <sefide36@tmon.co.kr>
2020-02-29 21:41:29 +0900

    fix : 교착 상태 해결

--- client/src/main/java/sjt/http/client/SjtWebClient.java
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -1,50 +1,28 @@
 package sjt.http.client;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class SjtWebClient {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
 
-        // 서버 접속요청
         Socket socket = new Socket("127.0.0.1", 8080);
-        InputStream inputStream = socket.getInputStream();
-        OutputStream outputStream = socket.getOutputStream();
 
-        // 서버에게 메세지 보내기
-        sendRequestMessage(outputStream);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        Thread.sleep(3000);
-        // 메세지 받기
-        getResponseMessage(inputStream);
+        bufferedWriter.write("GET /index.html HTTP/1.1 \r\n");
+        bufferedWriter.flush();
 
-        // 종료
-        Thread.sleep(1000);
+        String response = bufferedReader.readLine();
+        System.out.println("[Server] : " + response);
 
-//        inputStream.close();
-//        outputStream.close();
-//        socket.close();
-        System.out.println("client close");
-    }
-
-    private static void getResponseMessage(InputStream inputStream) throws IOException {
-        System.out.println("get Message.. - " + System.currentTimeMillis());
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        System.out.println(bufferedReader.readLine());
-    }
-
-    private static void sendRequestMessage(OutputStream outputStream) throws IOException {
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
-        bufferedOutputStream.write("GET /index.html HTTP/1.1".getBytes());
-        bufferedOutputStream.flush();
-        System.out.println("send Message..");
-        bufferedOutputStream.close();
+        socket.close();
     }
 
 }
