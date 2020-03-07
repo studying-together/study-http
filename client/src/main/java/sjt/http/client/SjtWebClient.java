cf2787359cc4e52a14db414cae007f4030ff53d4
homelus <homelus@daum.net>
2020-03-08 00:30:10 +0900

    add simple example

--- client/src/main/java/sjt/http/client/SjtWebClient.java
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -1,20 +1,34 @@
 package sjt.http.client;
 
+import com.squareup.okhttp.OkHttpClient;
+
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SjtWebClient {
 
     public static void main(String[] args) throws IOException {
 
-        Socket socket = new Socket("127.0.0.1", 8081);
+        OkHttpClient client = new OkHttpClient();
+        HttpURLConnection connection = client.open(new URL("https://www.naver.com"));
+        String contentType = connection.getContentType();
+        System.out.println(contentType);
+        System.out.println(connection.getResponseCode());
+        System.out.println(connection.getResponseMessage());
+        System.out.println(connection.getHeaderFields().toString());
 
-        final OutputStream out = socket.getOutputStream();
-        out.write("GET /index.html HTTP/1.1".getBytes());
+        InputStream inputStream = connection.getInputStream();
+        byte[] buffer = new byte[512];
+        StringBuilder sb = new StringBuilder();
+        while (inputStream.read(buffer) != -1) {
+            sb.append(new String(buffer));
+        }
+        System.out.println(sb.toString());
 
 
-        socket.close();
+
     }
 
 }
