d4109ccdd7b539c016c4d6e8339ff0a9e25870da
homelus <homelus@daum.net>
2020-03-25 13:37:26 +0900

    remove cloning files

--- client/src/main/java/sjt/http/client/SjtWebClient.java
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -1,9 +1,10 @@
 package sjt.http.client;
 
+import com.squareup.okhttp.OkHttpClient;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -11,12 +12,23 @@ public class SjtWebClient {
 
     public static void main(String[] args) throws IOException {
 //        jdkClient();
-//        useOkHttpClient();
-        useCloneOkHttpClient();
+        useOkHttpClient();
     }
 
-    private static void useCloneOkHttpClient() throws IOException {
-        sjt.http.client.clone.OkHttpClient client = new sjt.http.client.clone.OkHttpClient();
+    private static void jdkClient() throws IOException {
+        URL url = new URL("http://localhost:8080/testabc");
+        URLConnection urlConnection = url.openConnection();
+        InputStream inputStream = urlConnection.getInputStream();
+        byte[] buffer = new byte[512];
+        StringBuilder sb = newgit a StringBuilder();
+        while (inputStream.read(buffer) != -1) {
+            sb.append(new String(buffer));
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void useOkHttpClient() throws IOException {
+        OkHttpClient client = new OkHttpClient();
         HttpURLConnection connection = client.open(new URL("http://localhost:8080/testabc"));
         String contentType = connection.getContentType();
         System.out.println(contentType);
@@ -31,36 +43,6 @@ public class SjtWebClient {
             sb.append(new String(buffer));
         }
         System.out.println(sb.toString());
-    }
-
-    private static void jdkClient() throws IOException {
-        URL url = new URL("http://localhost:8080/testabc");
-        URLConnection urlConnection = url.openConnection();
-        InputStream inputStream = urlConnection.getInputStream();
-        byte[] buffer = new byte[512];
-        StringBuilder sb = new StringBuilder();
-        while (inputStream.read(buffer) != -1) {
-            sb.append(new String(buffer));
-        }
-        System.out.println(sb.toString());
-    }
-
-    private static void useOkHttpClient() throws IOException {
-//        OkHttpClient client = new OkHttpClient();
-//        HttpURLConnection connection = client.open(new URL("http://localhost:8080/testabc"));
-//        String contentType = connection.getContentType();
-//        System.out.println(contentType);
-//        System.out.println(connection.getResponseCode());
-//        System.out.println(connection.getResponseMessage());
-//        System.out.println(connection.getHeaderFields().toString());
-//
-//        InputStream inputStream = connection.getInputStream();
-//        byte[] buffer = new byte[512];
-//        StringBuilder sb = new StringBuilder();
-//        while (inputStream.read(buffer) != -1) {
-//            sb.append(new String(buffer));
-//        }
-//        System.out.println(sb.toString());
     }
 
 }
