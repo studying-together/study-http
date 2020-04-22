9926a74fcb5093fb24eae6ebee5a1a572d6116d4
homelus <homelus@daum.net>
2020-04-22 14:43:57 +0900

    update example

--- client/src/main/java/sjt/http/client/TyWebClient.java
+++ client/src/main/java/sjt/http/client/TyWebClient.java
@@ -1,22 +1,31 @@
 package sjt.http.client;
 
+import sjt.http.client.core.HttpClient;
+import sjt.http.client.core.Method;
+import sjt.http.client.core.Response;
+
+import static sjt.http.client.core.Method.*;
+
 public class TyWebClient implements WebClient {
 
+    HttpClient httpClient = new HttpClient();
+
     public <T> T get(String host, int port, String path, Class<T> clazz) {
+        Response response = httpClient.execute(GET, host + port + path, null);
         return null;
     }
 
     public <T> T post(String host, int port, String path, String body, Class<T> clazz) {
+        Response response = httpClient.execute(POST, host + port + path, body);
         return null;
     }
 
     public void put(String host, int port, String path, String body) {
-
+        httpClient.execute(PUT, host + port + path, body);
     }
 
     public void delete(String host, int port, String path) {
-
+        httpClient.execute(DELETE, host + port + path, null);
     }
-
 
 }
--- client/src/main/java/sjt/http/client/core/HttpClient.java
+++ client/src/main/java/sjt/http/client/core/HttpClient.java
@@ -1,4 +1,12 @@
 package sjt.http.client.core;
 
+import java.net.Socket;
+
 public class HttpClient {
+
+    Socket socket = new Socket();
+
+    public Response execute(Method method, String url, String body) {
+        return null;
+    }
 }
