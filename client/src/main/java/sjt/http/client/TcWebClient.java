7252c05eebc41ee77a572f6b83ac5735e3da6d52
homelus <homelus@daum.net>
2020-04-22 14:30:24 +0900

    init tc

--- client/src/main/java/sjt/http/client/TcWebClient.java
+++ client/src/main/java/sjt/http/client/TcWebClient.java
@@ -1,6 +1,6 @@
 package sjt.http.client;
 
-public class SimpleWebClient implements WebClient {
+public class TcWebClient implements WebClient {
     public <T> T get(String host, int port, String path, Class<T> clazz) {
         return null;
     }
