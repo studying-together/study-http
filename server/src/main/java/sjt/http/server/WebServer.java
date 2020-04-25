f6028ef0b99b087cb7633aad9a9a28e33a1cec82
sefide <sefide@naver.com>
2020-04-25 15:02:59 +0900

    TestServer 생성 및 tomcat 활용 실패..

--- server/src/main/java/sjt/http/server/WebServer.java
+++ server/src/main/java/sjt/http/server/WebServer.java
@@ -1,5 +1,11 @@
 package sjt.http.server;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.ServletException;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -9,11 +15,20 @@ import java.util.concurrent.Executors;
 
 public class WebServer {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ServletException, LifecycleException {
 
-        int port = 8080;
-        ServerSocket serverSocket = new ServerSocket(port);
+//        int port = 8080;
+//        ServerSocket serverSocket = new ServerSocket(port);
 
+        Tomcat tomcat = new Tomcat();
+        String webPort = "8080";
+        tomcat.setPort(Integer.parseInt(webPort));
+
+        Connector connector = tomcat.getConnector();
+        connector.setURIEncoding("UTF-8");
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 
 }
