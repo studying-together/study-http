53f1642cba467ec10adb2af72cd2d04e31e0f1b7
김다희 <sefide36@tmon.co.kr>
2020-04-25 13:52:18 +0900

    feat: TcHttpClient 생성

---
+++ client/src/main/java/sjt/http/client/TcHttpClient.java
@@ -0,0 +1,74 @@
+package sjt.http.client;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import sjt.http.server.servlet.Request;
+import sjt.http.server.servlet.Response;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+/**
+ * 실제 통신 수행
+ */
+public class TcHttpClient extends HttpClient {
+    private Socket socket;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
+    private Request request;
+    private Response response;
+
+    // executes HTTP request
+    public Response execute(HttpMethod httpMethod) {
+        sendRequest(httpMethod);
+
+        readResponse();
+        return new Response();
+    }
+
+    private void sendRequest(HttpMethod httpMethod) {
+        initTcHttpClient();
+
+        try {
+            sendRequestHeader(httpMethod);
+
+            String method = httpMethod.getName();
+            if (method.equals("GET")) {
+                // GET
+            } else if (method.equals("POST")) {
+                // POST
+            } else {
+                // HEAD, PUT ..
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void readResponse() {
+    }
+
+
+    // socket, writer/reader 초기화
+    private void initTcHttpClient() {
+        try {
+            socket = new Socket(sessionHost, sessionPort);
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- client/src/main/java/sjt/http/client/TcWebClient.java
+++ client/src/main/java/sjt/http/client/TcWebClient.java
@@ -1,12 +1,30 @@
 package sjt.http.client;
 
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import sjt.http.server.servlet.Response;
+
 public class TcWebClient implements WebClient {
+
+    private TcHttpClient tcHttpClient = new TcHttpClient();
+
     public <T> T get(String host, int port, String path, Class<T> clazz) {
-        return null;
+        tcHttpClient.startSession(host, port);
+        GetMethod getMethod = new GetMethod(path);
+
+        Response response = tcHttpClient.execute(getMethod);
+
+        return (T) response;
     }
 
     public <T> T post(String host, int port, String path, String body, Class<T> clazz) {
-        return null;
+        tcHttpClient.startSession(host, port);
+        PostMethod postMethod = new PostMethod(path);
+        postMethod.addParameter("body", body);
+
+        Response response = tcHttpClient.execute(postMethod);
+
+        return (T) response;
     }
 
     public void put(String host, int port, String path, String body) {
