eea02bede8f26fc690466c62fd13fd8bf4e9f835
홍성민 <honggasm@gmail.com>
2020-04-27 01:20:39 +0900

    [week1-sungmin] 시작줄, 헤더, 바디를 보낼 수 있는 인터페이스 추가

--- client/src/main/java/sjt/http/client/TcHttpClient.java
+++ client/src/main/java/sjt/http/client/TcHttpClient.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TcHttpClient {
     private static final String HTTP_VERSION = "HTTP/1.1";
-    private static final String CR = "\r\n";
+    private static final String CRLF = "\r\n";
     private static final int MTU = 1500;
     private final ObjectMapper objectMapper;
     private Connection connection;
@@ -33,16 +34,46 @@ public class TcHttpClient {
         return readResponse();
     }
 
+    //TODO:: write, read, 헤더초기화를 담당하는 로직을 HttpEngine 으로 분리해보는건 어떨까요?
     private void sendRequest(final Request request) {
-        // TODO : header, body write
         try {
             connect(request.getHost(), request.getPort());
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOut()), MTU);
-            writer.write(request.getMethod().name() + " " + request.getPath() + " " + HTTP_VERSION + CR);
+            initHeader(request);
+            writeStartLine(writer, request);
+            writeHeader(writer, request);
+            writeBody(writer, request);
             writer.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void initHeader(final Request request) {
+        //TODO:: 헤더 초기화 하는 로직 추가 필요
+    }
+    private void writeStartLine(final BufferedWriter writer,final Request request) throws IOException {
+        writer.write(request.getMethod().name() + " " + request.getPath() + " " + HTTP_VERSION + CRLF);
+    }
+
+    private void writeHeader(final BufferedWriter writer, final Request request) throws IOException {
+        final Map<String, String> header = request.getHeaders();
+        if(header == null) {
+            return;
+        }
+        for (String key : header.keySet()) {
+            writer.write(key + ": " + header.get(key));
+        }
+    }
+
+    private void writeBody(final BufferedWriter writer, final Request request) throws IOException {
+        if(isWriteable(request)) {
+            writer.write(request.getBody() + CRLF);
+        }
+    }
+
+    private boolean isWriteable(final Request request) {
+        return request.getBody() != null && HttpMethod.isBodyMethodType(request.getMethod());
     }
 
     /**
