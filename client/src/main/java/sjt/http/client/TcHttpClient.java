569fc63faf9e04eb492c9b143db33c550511a68e
sefide <sefide@naver.com>
2020-04-26 11:48:46 +0900

    test: 테스트 서버 추가

--- client/src/main/java/sjt/http/client/TcHttpClient.java
+++ client/src/main/java/sjt/http/client/TcHttpClient.java
@@ -14,6 +14,8 @@ import java.net.Socket;
  * 실제 통신 수행
  */
 public class TcHttpClient {
+    private static final String HTTP_VERSION = "HTTP/1.1";
+
     private Request request;
     private Response response;
     private BufferedReader reader;
@@ -30,6 +32,14 @@ public class TcHttpClient {
 
         // TODO : request 생성
         // TODO : header, body write
+        try {
+            writer.write(httpMethod.name() + " " + path + " " + HTTP_VERSION);
+            writer.flush();
+
+        } catch (IOException e) {
+//            e.printStackTrace();
+            throw new RuntimeException("request write 실패...");
+        }
     }
 
     private <T> T readResponse() {
@@ -44,7 +54,8 @@ public class TcHttpClient {
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         } catch (IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
+            throw new RuntimeException("socket 이슈 발생...");
         }
     }
 
--- server/src/main/java/sjt/http/server/WebServer.java
+++ server/src/main/java/sjt/http/server/WebServer.java
@@ -5,24 +5,38 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import sjt.http.server.servlet.TcHttpServlet;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class WebServer {
 
-    public static void main(String[] args) throws LifecycleException {
+    private static final int THREAD_POOL_SIZE = 4;
 
-//        int port = 8080;
-//        ServerSocket serverSocket = new ServerSocket(port);
+    public static void main(String[] args) throws LifecycleException, IOException {
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        Context context = tomcat.addContext("/","/");
+        int port = 8080;
+        ServerSocket serverSocket = new ServerSocket(port);
 
-        TcHttpServlet servlet = new TcHttpServlet();
-        String servletName = "TcServlet";
-        tomcat.addServlet("/", servletName, servlet);
-        context.addServletMapping("/user", servletName);
+        while(true) {
+            Socket socket = serverSocket.accept();
+            executorService.execute(() -> new ServerExecutor(socket).execute());
+        }
 
-        tomcat.start();
-        tomcat.getServer().await();
+//        Tomcat tomcat = new Tomcat();
+//        tomcat.setPort(8080);
+//        Context context = tomcat.addContext("/","/");
+//
+//        TcHttpServlet servlet = new TcHttpServlet();
+//        String servletName = "TcServlet";
+//        tomcat.addServlet("/", servletName, servlet);
+//        context.addServletMapping("/user", servletName);
+//
+//        tomcat.start();
+//        tomcat.getServer().await();
     }
 
 }
