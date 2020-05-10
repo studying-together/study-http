262509fafa2592643da97d37dc8894de0fbd70e8
jungmisu <jungmisu@tmon.co.kr>
2020-05-10 17:31:06 +0900

    string controller 추가

--- server/src/main/java/sjt/http/server/WebServer.java
+++ server/src/main/java/sjt/http/server/WebServer.java
@@ -1,10 +1,14 @@
 package sjt.http.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @SpringBootApplication
 @Controller
 public class WebServer {
@@ -17,5 +21,11 @@ public class WebServer {
     @GetMapping(value = "/hello", produces = "text/html")
     public String getHello() {
         return "hello";
+    }
+
+    @GetMapping(value = "/{message}", produces = "text/plain")
+    public @ResponseBody String getMessage(@PathVariable String message) {
+        log.info("get Message : {}" , message);
+        return message;
     }
 }
