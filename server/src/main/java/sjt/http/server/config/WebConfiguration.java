5d95ea7167c6f55dd71e3e41fa27aa16458afe19
jungmisu <jungmisu@tmon.co.kr>
2020-05-10 10:07:46 +0900

    server단 spring boot로 전환

---
+++ server/src/main/java/sjt/http/server/config/WebConfiguration.java
@@ -0,0 +1,14 @@
+package sjt.http.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
